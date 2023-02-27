fd5bc7e507028522fd55c48943b9bf67e6249a3d
Viktor Pechko <vppechko@gmail.com>
2023-02-27 16:42:15 +0300

    Добавьте файлы проекта.

---
+++ DistributionOfStudents/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DistributionOfStudents.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
