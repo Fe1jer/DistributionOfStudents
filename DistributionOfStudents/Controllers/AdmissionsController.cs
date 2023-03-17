81f62a4fc5b38f5aa7cebfc4da3845596efddb13
Viktor Pechko <vppechko@gmail.com>
2023-03-18 00:56:23 +0300

    adding statistics calculation

--- DistributionOfStudents/Controllers/AdmissionsController.cs
+++ DistributionOfStudents/Controllers/AdmissionsController.cs
@@ -1,9 +1,13 @@
-﻿using DistributionOfStudents.Data.Interfaces;
+﻿using ChartJSCore.Models;
+using DistributionOfStudents.Data.Interfaces;
 using DistributionOfStudents.Data.Models;
+using DistributionOfStudents.Data.Services;
 using DistributionOfStudents.Data.Specifications;
 using DistributionOfStudents.ViewModels.Admissions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace DistributionOfStudents.Controllers
 {
@@ -13,17 +17,22 @@ namespace DistributionOfStudents.Controllers
         private readonly ILogger<AdmissionsController> _logger;
         private readonly IAdmissionsRepository _admissionsRepository;
         private readonly IGroupsOfSpecialitiesRepository _groupsRepository;
-        private readonly IRecruitmentPlansRepository _planRepository;
+        private readonly IRecruitmentPlansRepository _plansRepository;
         private readonly ISubjectsRepository _subjectsRepository;
+        private readonly IGroupsOfSpecialitiesStatisticRepository _groupsStatisticRepository;
+        private readonly IRecruitmentPlansStatisticRepository _plansStatisticRepository;
 
         public AdmissionsController(ILogger<AdmissionsController> logger, IAdmissionsRepository admissionsRepository, IGroupsOfSpecialitiesRepository groupsRepository,
-            IRecruitmentPlansRepository planRepository, ISubjectsRepository subjectsRepository)
+            IRecruitmentPlansRepository plansRepository, ISubjectsRepository subjectsRepository,
+            IGroupsOfSpecialitiesStatisticRepository groupsStatisticRepository, IRecruitmentPlansStatisticRepository plansStatisticRepository)
         {
             _logger = logger;
             _admissionsRepository = admissionsRepository;
             _groupsRepository = groupsRepository;
-            _planRepository = planRepository;
+            _plansRepository = plansRepository;
             _subjectsRepository = subjectsRepository;
+            _groupsStatisticRepository = groupsStatisticRepository;
+            _plansStatisticRepository = plansStatisticRepository;
         }
 
         // GET: Admissions/Details/5
@@ -50,7 +59,7 @@ namespace DistributionOfStudents.Controllers
             {
                 return NotFound();
             }
-            List<RecruitmentPlan> plans = await _planRepository.GetAllAsync(new RecruitmentPlansSpecification().WhereFaculty(facultyName).WhereGroup(group));
+            List<RecruitmentPlan> plans = await _plansRepository.GetAllAsync(new RecruitmentPlansSpecification().WhereFaculty(facultyName).WhereGroup(group));
             plans = plans.Where(p => (group.Specialities ?? new()).Contains(p.Speciality)).ToList();
             (group.Subjects ?? new()).ForEach(subject => scores.Add(new StudentScore() { Subject = subject }));
             plans.ForEach(plan => priorities.Add(new SpecialityPriorityVM(plan)));
@@ -82,7 +91,7 @@ namespace DistributionOfStudents.Controllers
                 }
                 foreach (SpecialityPriorityVM specialityPriority in model.SpecialitiesPriority.Where(p => p.Priority > 0))
                 {
-                    RecruitmentPlan? plan = await _planRepository.GetByIdAsync(specialityPriority.PlanId);
+                    RecruitmentPlan? plan = await _plansRepository.GetByIdAsync(specialityPriority.PlanId);
                     if (plan != null)
                     {
                         SpecialityPriority priority = new() { Priority = specialityPriority.Priority, RecruitmentPlan = plan };
@@ -90,6 +99,7 @@ namespace DistributionOfStudents.Controllers
                     }
                 }
 
+                GroupOfSpecialties group = await _groupsRepository.GetByIdAsync(groupId, new GroupsOfSpecialitiesSpecification().IncludeAdmissions()) ?? new();
                 Admission admission = new()
                 {
                     GroupOfSpecialties = await _groupsRepository.GetByIdAsync(groupId) ?? new(),
@@ -98,9 +108,8 @@ namespace DistributionOfStudents.Controllers
                     DateOfApplication = model.DateOfApplication,
                     SpecialityPriorities = specialityPriorities
                 };
-
                 await _admissionsRepository.AddAsync(admission);
-
+                await UpdateStatisticAsync(facultyName, groupId);
                 _logger.LogInformation("Заявка студента - {Surname} {Name} {Patronymic} - добавлена", admission.Student.Surname, admission.Student.Name, admission.Student.Patronymic);
 
                 return RedirectToAction("Details", "GroupsOfSpecialties", new { facultyName, id = groupId });
@@ -119,7 +128,7 @@ namespace DistributionOfStudents.Controllers
                 return NotFound();
             }
 
-            List<RecruitmentPlan> plans = await _planRepository.GetAllAsync(new RecruitmentPlansSpecification().WhereFaculty(facultyName).WhereGroup(group));
+            List<RecruitmentPlan> plans = await _plansRepository.GetAllAsync(new RecruitmentPlansSpecification().WhereFaculty(facultyName).WhereGroup(group));
             plans = plans.Where(p => (group.Specialities ?? new()).Contains(p.Speciality)).ToList();
             priorities = GetSpecialityPrioritiesVM(plans, admission);
 
@@ -153,7 +162,7 @@ namespace DistributionOfStudents.Controllers
                 foreach (SpecialityPriorityVM specialityPriority in model.SpecialitiesPriority.Where(p => p.Priority > 0))
                 {
                     SpecialityPriority priority = admission.SpecialityPriorities.FirstOrDefault(i => i.RecruitmentPlan.Id == specialityPriority.PlanId) ??
-                        new() { RecruitmentPlan = await _planRepository.GetByIdAsync(specialityPriority.PlanId) ?? new() };
+                        new() { RecruitmentPlan = await _plansRepository.GetByIdAsync(specialityPriority.PlanId) ?? new() };
                     priority.Priority = specialityPriority.Priority;
                     specialityPriorities.Add(priority);
                 }
@@ -193,6 +202,7 @@ namespace DistributionOfStudents.Controllers
             if (admission != null)
             {
                 await _admissionsRepository.DeleteAsync(id);
+                await UpdateStatisticAsync(facultyName, groupId);
                 _logger.LogInformation("Заявка студента - {Surname} {Name} {Patronymic} - была удалена", admission.Student.Surname, admission.Student.Name, admission.Student.Patronymic);
             }
 
@@ -203,6 +213,37 @@ namespace DistributionOfStudents.Controllers
         {
             var admissions = await _admissionsRepository.GetAllAsync();
             return admissions.Any(e => e.Id == id);
+        }
+
+        private async Task UpdateStatisticAsync(string facultyName, int groupId)
+        {
+            GroupOfSpecialties group = await _groupsRepository.GetByIdAsync(groupId, new GroupsOfSpecialitiesSpecification().IncludeSpecialties().IncludeAdmissions()) ?? new();
+            await UpdateGroupStatisticAsync(group);
+            await UpdatePlansStatisticAsync(facultyName, group);
+        }
+
+        private async Task UpdateGroupStatisticAsync(GroupOfSpecialties group)
+        {
+            GroupOfSpecialitiesStatistic groupStatistic = await _groupsStatisticRepository.GetByGroupAndDateAsync(group.Id, DateTime.Today)
+                ?? new() { Date = DateTime.Today, GroupOfSpecialties = group };
+            groupStatistic.CountOfAdmissions = (group.Admissions ?? new()).Count;
+            Task task = groupStatistic.Id != 0 ? _groupsStatisticRepository.UpdateAsync(groupStatistic) : _groupsStatisticRepository.AddAsync(groupStatistic);
+            await task;
+        }
+
+        private async Task UpdatePlansStatisticAsync(string facultyName, GroupOfSpecialties group)
+        {
+            List<RecruitmentPlan> plans = await _plansRepository.GetAllAsync(new RecruitmentPlansSpecification().WhereFaculty(facultyName).WhereGroup(group));
+            DistributionService distributionService = new(plans, group.Admissions);
+            plans = distributionService.GetPlansWithEnrolledStudents();
+            foreach (var plan in plans)
+            {
+                RecruitmentPlanStatistic planStatistic = await _plansStatisticRepository.GetByPlanAndDateAsync(plan.Id, DateTime.Today)
+                    ?? new() { Date = DateTime.Today, RecruitmentPlan = await _plansRepository.GetByIdAsync(plan.Id) ?? new() };
+                planStatistic.Score = plan.PassingScore;
+                Task task = planStatistic.Id != 0 ? _plansStatisticRepository.UpdateAsync(planStatistic) : _plansStatisticRepository.AddAsync(planStatistic);
+                await task;
+            }
         }
 
         private static List<SpecialityPriorityVM> GetSpecialityPrioritiesVM(List<RecruitmentPlan> plans, Admission admission)
